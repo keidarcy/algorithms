1b251f39e74b4bd9340b14b90f2e58acaf6f3e3e
Xing Yahao <keidarcy.1015@gmail.com>
2023-01-01 10:16:37 +0900

    chore(java): merge sort

--- java-algorithms/src/Main.java
+++ java-algorithms/src/Main.java
@@ -1,5 +1,6 @@
 import algorithm.BubbleSort;
 import algorithm.InsertionSort;
+import algorithm.MergeSort;
 import algorithm.SelectionSort;
 
 import java.util.Arrays;
@@ -9,7 +10,8 @@ public class Main {
         int[] numbers = {3, 3, 1 ,54 ,6, 3, 2};
 //        var sorter = new BubbleSort();
 //        var sorter = new SelectionSort();
-        var sorter = new InsertionSort();
+//        var sorter = new InsertionSort();
+        var sorter = new MergeSort();
         sorter.sort(numbers);
         System.out.println(Arrays.toString(numbers));
     }
